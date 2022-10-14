e81ca53a685119f5d821740db1a3761a195249cc
Александр Салмин <salmin.alex98@gmail.com>
2022-10-14 16:49:15 +0300

    Второй коммит

--- main.js
+++ main.js
@@ -1,3 +1,46 @@
-console.log('Здравствуйте, Тимур')
-console.log(2+2)
-console.log(32*15)
+const myBirthdayMonth = "9";
+console.log(myBirthdayMonth);
+
+alert ('14.10.22')
+
+let str = 'Добро';
+let str1 = "пожаловать";
+let str2 = 'на курс'
+let str3 = `${str} ${str1} ${str2} Александр`; //String(строка)
+console.log(str3);
+console.log(typeof str3)
+
+document.getElementById("one").innerHTML = "Hello world, everyone";
+
+var a; // Старьё
+let b; // Современная тема
+const c = 0; // Вечная штука
+
+let happyNewYear = "31.12.2022";
+happyNewYear = '31.12.2023';
+console.log(happyNewYear); // Горбатый регистр
+
+
+let num = 15; //Number
+let num1 = 915051215464123100818471876513n;//bigint
+console.log(num1);
+console.log(typeof num1)
+
+// let DotCheck = false; //
+// let DotCheck = true; //
+
+const nul = null;
+console.log(nul);
+console.log(typeof nul);
+
+let und = undefined;
+console.log(und);
+console.log(typeof und);
+
+let obj = {};
+console.log(typeof obj)
+
+
+
+
+
